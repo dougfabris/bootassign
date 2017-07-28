a11a7bc9a626244316d9d46ca0b5df5be32524ec
Roberth Godoy <roberth.godoy@yahoo.com>
2017-07-28 17:22:23 +0000

    fix css

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,24 +4,12 @@ function scrollDown() {
   }, 1000);
 }
 
-/*function checkAnimation(elem) {
-  if (isScrolledIntoView($(elem))) {
-    $('#fixed-header').css('display', 'block');
-  }
-  else
-    $('#fixed-header').css('display', 'none');
-}*/
-
 function isScrolledIntoView(elem) {
   var distanciaDaTelaComTopo = $(window).scrollTop();
   var alturaDaLanding = $(elem).outerHeight();
 
   return (distanciaDaTelaComTopo >= alturaDaLanding);
 }
-
-$(window).scroll(function() {
-  //checkAnimation($('.p-hero'));
-});
 
 $('#headerButton #talkButton').click(function() {
   $.post('/converse-comigo');
