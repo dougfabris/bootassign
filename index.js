86ec90d42f51083addac9e5e3683028c88a64e49
Roberth Godoy <roberth.godoy@yahoo.com>
2017-07-28 14:02:17 +0000

    conect

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict'
 
-const express = require('express')
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
+
+/*const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 var path = require('path')
@@ -34,3 +40,4 @@ app.get('/', function(req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+*/
